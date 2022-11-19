32441eed0db8d15ceca363b654dd01b6eec95c5e
Home <evgeniakrig@gmail.com>
2022-11-19 19:14:52 +0300

    Add class object IStringFormatter

---
+++ StringFormatter/StringFormatter/StringFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormatter
+{
+    public class StringFormatter : IStringFormatter
+    {
+        public static readonly StringFormatter Shared = new StringFormatter();
+
+        public StringFormatter()
+        {
+        }
+
+        public string Format(string template, object target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
